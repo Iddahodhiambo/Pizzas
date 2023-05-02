a0726137db704b95d1b0af751f958e18fe0b3c3a
Iddahodhiambo <iddahawuor@yahoo.com>
2023-05-02 23:11:57 +0300

    get restaurant by id

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,4 +4,14 @@ class RestaurantsController < ApplicationController
        restaurants = Restaurant.all 
        render json: restaurants
     end
+
+  def show
+    restaurant = Restaurant.find_by(id: params[:id])
+    if restaurant
+        render json: restaurant
+    else 
+        render json: {error: "Restaurant not found"}, status: :not_found
+    end
+end
+
 end
