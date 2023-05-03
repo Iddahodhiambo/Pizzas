b7ae16da18ff9ecf40238c54df7944bf3f89a4c0
Iddahodhiambo <iddahawuor@yahoo.com>
2023-05-03 19:16:42 +0300

    delete a restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,5 +13,15 @@ class RestaurantsController < ApplicationController
         render json: {error: "Restaurant not found"}, status: :not_found
     end
 end
+ def destroy
+    
+    restaurant = Restaurant.find_by(id: params[:id])
+    if restaurant
+        restaurant.destroy
+        head :no_content
+    else 
+        render json: {error: "Restaurant not found"}, status: :not_found
+    end
 
+ end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,17 @@
 Pizza.create(name:"peperoni",ingredients:"chicken and beef")
 Pizza.create(name:"Margerita",ingredients:"cheese and beef")
 Pizza.create(name:"Veggie pizza",ingredients:"vegetable and fruits")
+Pizza.create(name:"Meghan pizza",ingredients:"vegetable and fish")
+Pizza.create(name:"cheese pizza",ingredients:"vegetable ,dough and fish")
+
+
 
 Restaurant.create(name:"Naliaz",address:"Nairobi")
 Restaurant.create(name:"Ashaki",address:"Nakuru")
 Restaurant.create(name:"Java",address:"Kisumu")
+Restaurant.create(name:"kosewe",address:"Mombasa")
+Restaurant.create(name:"Freds",address:"Kajiado")
+
+
 
 
